edbcf5c10b6c73e9610fa48ce137c995f9268a40
anton <Tiramisouls06@gmail.com>
2023-04-28 10:50:43 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,19 @@
-/*
-Добро пожаловать. Это многолинейный комментарий. Пишем что хотим и сколько хотим.
- */
 
-import java.util.Scanner;
-
-// Главная реализация моего объекта это Класс
-class Main
-{
-    public static void main(String[] args)
-    {
-        // Этот код выводит сообщение, one-line comment
-        // Legacy Code - код написанный другим программистом и скорее всего
-        // на старой версии языка
-        // Java - это чаще всего 1.8 (Java 8)
-
-        System.out.print("");
+public class Main {
+    public static void main(String[] args) {
+        // Вывод фигуры "ёлочка "
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
         System.out.println();
-
-        Scanner user_input = new Scanner(System.in, "Cp1251");
-
-        // new line - symbol, \n
-
-        // This is an example comment for Git
-        String first_name;
-        System.out.print("введите имя: ");
-        first_name = user_input.next();
-
-        String family_name;
-        System.out.print("введите фамилию: ");
-        family_name = user_input.next();
-
-        String full;
-        full = first_name + " " + family_name;
-
-        System.out.println("вас зовут " + full);
-
+        //Вывод поля для игры "Кристики-нолики"
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("____|____|____");
+        System.out.println("    |    |    ");
+        System.out.println("    |    |    ");
     }
 }
