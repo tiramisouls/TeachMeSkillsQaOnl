8d9adf43d22bc8d0b798a1372603160d27e837e8
anton <Tiramisouls06@gmail.com>
2023-05-28 19:58:45 +0300

    commit 19

---
+++ src/Authorization/WrongLoginException.java
@@ -0,0 +1,11 @@
+package Authorization;
+
+public class WrongLoginException  extends Exception
+{
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
